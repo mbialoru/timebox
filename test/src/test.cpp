ab4996abffed8ae2841dc197849b54badd0ee734
Marek Białoruski <mar.bialoruski@gmail.com>
2022-07-09 13:12:48 +0000

    PID Test

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,20 +1,36 @@
 #include <gtest/gtest.h>
+#include "pid.hpp"
 
 
-class TestTemplate : public ::testing::Test
+class PIDTest : public ::testing::Test
 {
 protected:
-  // Here initiate objects used in tests
+  PID<double> pid{ 1.2, 1.0, 0.001, 0 };
+
+  double temperature(double t = 0.0)
+  {
+    // Could be its own class, but this is more compact
+    static double _temperature{ 15 };
+    _temperature += t / 4;
+    return _temperature;
+  }
+
 public:
-  // SetUp is called before every test
-  // TearDown is called after every test
   void SetUp() override {}
   void TearDown() override {}
 };
 
-TEST_F(TestTemplate, EmptyTest) {}
-
-TEST_F(TestTemplate, SkippedTest)
+TEST_F(PIDTest, Control_Temperature)
 {
-  GTEST_SKIP() << "Skipping single test";
+  for (std::size_t i = 0; i < 50; i++)
+  {
+    if (i >= 9)
+    {
+      pid.set_target(60);
+      pid.update(this->temperature(), 1);
+    }
+    if (pid.get_target() > 0)
+      this->temperature(pid.get_output() - 1 / i);
+  }
+  EXPECT_NEAR(this->temperature(), 60, 0.01);
 }
