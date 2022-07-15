b028e0ea94b49b97b2d4a28368f35336e095e35b
Marek Białoruski <mar.bialoruski@gmail.com>
2022-07-15 12:20:13 +0000

    C++ Class generator tool

---
+++ utils/cpp_class.py
@@ -0,0 +1,29 @@
+import sys
+
+ """ Small script to generate header and implementation file for C++ class """
+
+def main():
+    name = str(sys.argv[1])
+    filename = name.replace(" ", "").lower()
+    with open(f"{filename}.hpp", "wt") as f:
+        f.write(f"#ifndef {name.upper()}_HPP\n")
+        f.write(f"#define {name.upper()}_HPP\n\n")
+        f.write("#pragma once\n\n")
+        f.write("// Includes here\n\n")
+        f.write(f"class {name}\n{{\n")
+        f.write("private:\n\n")
+        f.write("public:\n")
+        f.write(f"  {name}();\n")
+        f.write(f"  ~{name}() = default;\n")
+        f.write("};\n\n")
+        f.write(f"#endif // {name.upper()}_HPP")
+
+    with open(f"{filename}.cpp", "wt") as f:
+        f.write(f"#include \"{filename}.hpp\"\n\n")
+        f.write(f"{name}::{name}() {{}};")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Invalid ammount of arguments")
+        sys.exit(1)
+    sys.exit(main() or 0)
