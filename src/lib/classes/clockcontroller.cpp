e0466418bfb3e68d5bb7606f5bea0da1399d4c58
Marek Białoruski <mar.bialoruski@gmail.com>
2022-07-18 15:02:04 +0000

    Modified ClockController stub

--- src/lib/classes/clockcontroller.cpp
+++ src/lib/classes/clockcontroller.cpp
@@ -1,1 +1,12 @@
 #include "clockcontroller.hpp"
+
+ClockController::ClockController()
+{
+  // 0 is the aimed at value of difference between system clock and PPS
+  pid = std::make_unique<PID<double>>(1.2, 1.0, 0.001, 0);
+
+  if (adjtimex(&buffer) != 0)
+    throw TimexAcquisitionError();
+
+  
+}
--- src/lib/classes/clockcontroller.hpp
+++ src/lib/classes/clockcontroller.hpp
@@ -3,25 +3,35 @@
 
 #pragma once
 
-#include <vector>
 #include <ctime>
+#include <memory>
+#include <sys/timex.h>
 #include "pid.hpp"
+#include "exceptions.hpp"
 
 class ClockController
 {
 public:
   ClockController();
   ~ClockController() = default;
+  void Trigger(char*);
 
 private:
+  int clock_mode;
+  timex buffer;
   struct tm curr { 0 };
   struct tm last { 0 };
+  std::unique_ptr<PID<double>> pid;
+  void AdjustKernelTick(unsigned);
+
+
   // TODO: Some container to store differences in PPS time and system clock time
   // Way to calculate these differences
   // Way to connect that with embedded PID object
   // Way to influence system clock speed (tick for unix - timex)
   // Way to access current data (history and last difference)
   // Preferably time measurements done on a separate thread ?
+  // No, we will make a trigger method, which will do all the work
 };
 
 
