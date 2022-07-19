eb9cfcf07cf0e671115354965b2a4dd232331208
Marek Białoruski <mar.bialoruski@gmail.com>
2022-07-19 21:04:47 +0000

    New exceptions

--- src/lib/exceptions.hpp
+++ src/lib/exceptions.hpp
@@ -17,11 +17,26 @@ private:
   std::string msg{};
 };
 
-
 class TimexAcquisitionError : public std::exception
 {
 public:
   TimexAcquisitionError(const char* msg = "Error during acquisition of timex struct")
+  {
+    this->msg = msg;
+  }
+  const char* what() const noexcept
+  {
+    return this->msg.c_str();
+  }
+
+private:
+  std::string msg{};
+};
+
+class TimexChangeError : public std::exception
+{
+public:
+  TimexChangeError(const char* msg = "Error during modifying timex struct")
   {
     this->msg = msg;
   }
