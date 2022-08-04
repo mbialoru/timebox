7c148ef6b34b09d23c0af58965bd8264823e8aac
Marek Białoruski <mar.bialoruski@gmail.com>
2022-08-04 10:32:29 +0000

    CMake changes and config

---
+++ app/lib/include/config.hpp
@@ -0,0 +1,10 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#define PROJECT_NAME "timebox"
+#define PROJECT_VER "0.1.0"
+#define PROJECT_VER_MAJOR "0"
+#define PROJECT_VER_MINOR "1"
+#define PTOJECT_VER_PATCH "0"
+
+#endif// CONFIG_HPP
