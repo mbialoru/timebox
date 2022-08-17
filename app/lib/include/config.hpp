b401e702c0de060450bf039affc8b2cb12026245
Marek Białoruski <mar.bialoruski@gmail.com>
2022-08-17 11:49:39 +0000

    CMake reconfiguring

--- app/lib/include/config.hpp
+++ app/lib/include/config.hpp
@@ -6,8 +6,9 @@
 
 static constexpr std::string_view PROJECT_NAME{ "timebox" };
 static constexpr std::string_view PROJECT_VERSION{ "0.1.0" };
-static constexpr std::string_view BUILD_INFO{
-  "timebox-0.1.0-564c2974-Linux-Debug"
+static constexpr std::string_view BUILD_INFO
+{
+  "timebox-0.1.0-43fee7cb-Linux-Debug"
   "-GNU-12.1.0"
 };
 
