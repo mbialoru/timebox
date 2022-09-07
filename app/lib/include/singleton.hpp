4203a561a5b40c59897b1ca752afb91e7881fd89
Marek Białoruski <mar.bialoruski@gmail.com>
2022-09-07 15:16:15 +0000

    Singleton draft

---
+++ app/lib/include/singleton.hpp
@@ -0,0 +1,26 @@
+#ifndef SINGLETON_HPP
+#define SINGLETON_HPP
+
+#pragma once
+
+// Includes here
+
+template<typename T>
+class Singleton
+{
+public:
+  static T& GetInstance() noexcept(std::is_nothrow_constructible<T>::value)
+  {
+    static T instance;
+    return instance;
+  }
+
+protected:
+  Singleton() noexcept = default;
+  Singleton(const Singleton&) = delete;
+  Singleton& operator=(const Singleton&) = delete;
+
+  virtual ~Singleton() = default;
+};
+
+#endif // SINGLETON_HPP
