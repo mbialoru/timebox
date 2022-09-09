3b71d38bfc9faf05a5a3002dea3989b7ca5a4b64
Marek Białoruski <mar.bialoruski@gmail.com>
2022-09-09 14:36:21 +0000

    Tests passing with faked objects

--- app/test/include/fakes.hpp
+++ app/test/include/fakes.hpp
@@ -1,30 +1,51 @@
 #ifndef TEST_FAKES_HPP
 #define TEST_FAKES_HPP
 
+#include "clockcontroller.hpp"
 #include "defines.hpp"
 #include "serialreader.hpp"
-#include "threadwrapper.hpp"
 #include "utilities.hpp"
 
-class FakeSerialReader : public TimeBox::ThreadWrapper
-{
-private:
-  std::function<void(TimeBox::TimeboxReadout)> m_callback = nullptr;
-  void Work() override
-  {
-    auto now = std::chrono::system_clock::now();
-    now -= std::chrono::milliseconds(200);
-    m_callback(TimeBox::TimeboxReadout{ TimeBox::ConvertTimepointToString(now), now });
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-  }
+namespace TimeBox {
 
+class FakeSerialReader : public SerialReader
+{
 public:
-  FakeSerialReader(const char *, std::size_t, std::function<void(TimeBox::TimeboxReadout)> t_callback)
-    : TimeBox::ThreadWrapper::ThreadWrapper("FakeSerialReader"), m_callback(t_callback)
+  FakeSerialReader(const char *, std::size_t, std::function<void(TimeboxReadout)> t_callback)
+    : SerialReader(std::move(t_callback))
   {
     m_is_paused = false;
   };
-  ~FakeSerialReader() = default;
+
+  ~FakeSerialReader()
+  {
+    WipeSerialBuffer();
+    m_callback = nullptr;
+    m_is_paused = true;
+  };
+
+  bool QuerryDevice(std::string) override { return true; };
+
+private:
+  void InitializeSerial(const char *, std::size_t) override{};
+  void Work() override
+  {
+    auto now = std::chrono::system_clock::now() - std::chrono::milliseconds(200);
+    m_callback(TimeboxReadout{ ConvertTimepointToString(now) + ".0", now });
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
 };
+
+class FakeClockController : public ClockController
+{
+public:
+  FakeClockController() : ClockController(500){};
+  ~FakeClockController() = default;
+
+  void AdjustClock(TimeboxReadout) override{};
+  std::chrono::system_clock::duration CalculateClockDifference() override{};
+};
+
+}// namespace TimeBox
 
 #endif// TEST_FAKES_HPP
--- app/test/src/test_combined.cpp
+++ app/test/src/test_combined.cpp
@@ -4,19 +4,16 @@
 
 #if USING_REAL_HARDWARE
 #if defined(__unix__)
-#include "serialreader.hpp"
-
-#elif defined(_WIN64) && !defined(__CYGWIN__)
-#endif
-
-#else
-#if defined(__unix__)
 #include "linclockcontroller.hpp"
+#include "linserialreader.hpp"
 #include "pid.hpp"
 
 #elif defined(_WIN64) && !defined(__CYGWIN__)
+#include "winclockconroller.hpp"
+#include "winserialreader.hpp"
 #endif
 
+#else
 #include "fakes.hpp"
 #endif
 
@@ -25,24 +22,25 @@ using namespace TimeBox;
 class Test_Combined : public ::testing::Test
 {
 public:
-  void SetUp() override{};
+  void SetUp() override
+  {
+    if (not LONG_TESTS) { GTEST_SKIP() << "Skipping, LONG_TESTS = " << LONG_TESTS; }
+  };
   void TearDown() override{};
 };
+
+TEST_F(Test_Combined, fake_combined_test)
+{
+  FakeClockController cc;
+  FakeSerialReader sr{ "/dev/ttyACM0", 9600, std::bind(&FakeClockController::AdjustClock, &cc, std::placeholders::_1) };
+  std::this_thread::sleep_for(std::chrono::seconds(10));
+}
 
 #if defined(__unix__)
 
 TEST_F(Test_Combined, linux_combined_test)
 {
-  if (!LONG_TESTS) GTEST_SKIP() << "Skipping, LONG_TESTS = " << LONG_TESTS;
-
-  std::shared_ptr<PID<double>> p_pid{ std::make_shared<PID<double>>(2.0, 1.0, 0.001, 0) };
-  LinClockController cc{ 0, p_pid, 0.001 };
-#if USING_REAL_HARDWARE
-  SerialReader sr{ "/dev/ttyACM0", 9600, std::bind(&ClockController::AdjustClock, &cc, std::placeholders::_1) };
-#else
-  FakeSerialReader sr{ "/dev/ttyACM0", 9600, std::bind(&LinClockController::AdjustClock, &cc, std::placeholders::_1) };
-#endif
-  std::this_thread::sleep_for(std::chrono::seconds(10));
+  if (not CheckAdminPrivileges()) { GTEST_SKIP() << "Skipping, requires admin privileges"; }
 }
 
 #elif defined(_WIN64) && !defined(__CYGWIN__)
