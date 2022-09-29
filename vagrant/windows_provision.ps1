6de5c2cd95a10af89982700e195190fd48392330
Marek Białoruski <mar.bialoruski@gmail.com>
2022-09-29 12:33:19 +0200

    VSCode server for windows box

--- vagrant/windows_provision.ps1
+++ vagrant/windows_provision.ps1
@@ -5,14 +5,14 @@ $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 
 # Allow for any scripts to run
-# Set-ExecutionPolicy Unrestricted -Scope LocalMachine
+Set-ExecutionPolicy Unrestricted -Scope LocalMachine
 
 # Make powershell the default ssh shell
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
 
 # Install packages via chocolatey
 choco install -y git
-choco install -y cmake.install --installargs '"ADD_CMAKE_TO_PATH=System"'
+choco install -y cmake.install --installargs '"ADD_CMAKE_TO_PATH=System"'serve
 choco install -y visualstudio2022buildtools --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools;includeRecommended;includeOptional --passive --locale en-US"
 choco install -y boost-msvc-14.3
 choco install -y doxygen.install
@@ -22,8 +22,16 @@ choco install -y doxygen.install
 New-item -type file -force $profile
 echo "& 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\Launch-VsDevShell.ps1' | out-null" > C:\Users\vagrant\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 
+# Install Visual Studio Code server for remote development
+New-Item "$HOME\.vscode-server-launcher\bin" -ItemType "directory" -Force
+Invoke-WebRequest "https://aka.ms/vscode-server-launcher/x86_64-pc-windows-msvc" -OutFile "$HOME\.vscode-server-launcher\bin\code-server.exe"
+[Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", "User") + ";$HOME\.vscode-server-launcher\bin", "User")
+
 # refreshenv is now an alias for Update-SessionEnvironment
 # (rather than invoking refreshenv.cmd, the *batch file* for use with cmd.exe)
 # This should make programs accessible via the refreshed $env:PATH, so that they
 # can be called by name only.
 refreshenv
+
+# Start Visual Studio Code server
+code-server serve --disable-telemetry
