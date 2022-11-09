b005f23816afe25bb7803ec33890a80382d5ad71
Marek Bialoruski <mar.bialoruski@gmail.com>
2022-11-09 04:25:12 -0800

    BuildInformation upgrade

--- app/lib/include/config.hpp
+++ app/lib/include/config.hpp
@@ -6,11 +6,20 @@
 
 namespace TimeBox {
 
-static constexpr std::string_view PROJECT_NAME{ "timebox" };
-static constexpr std::string_view PROJECT_VERSION{ "0.1.0" };
-static constexpr std::string_view BUILD_INFO{
-  "timebox-0.1.0-e8dd5055-Windows-Debug"
-  "-GNU-12.2.0"
+struct BuildInformation
+{
+  static constexpr std::string_view PROJECT_NAME{ "timebox" };
+  static constexpr std::string_view PROJECT_VERSION{ "0.0.15" };
+  static constexpr std::string_view PROJECT_VERSION_ADDENDUM{ "InDev" };
+  static constexpr std::string_view GIT_BRANCH{ "dev-windows" };
+  static constexpr std::string_view GIT_BRANCH_STATE{ "DIRTY" };
+  static constexpr std::string_view GIT_SHORT_SHA{ "aa333824" };
+  static constexpr std::string_view BUILD_TYPE{ "Debug" };
+  static constexpr std::string_view PLATFORM{ "Windows" };
+  static constexpr std::string_view COMPILER{ "GNU" };
+  static constexpr std::string_view COMPILER_VERSION{ "12.2.0" };
+  static constexpr std::string_view BUILD_DATE{ "2022-11-09 12:23:44 UTC" };
+  static constexpr std::string_view BUILD_HASH{ "90b58801daf13791d1702f08fb8fe5a14c898840a5c95167a29c245bf2b87bfe" };
 };
 
 static constexpr std::size_t WINDOW_HEIGHT{ 600 };
