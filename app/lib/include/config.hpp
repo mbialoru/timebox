95669aa8440c81f4ee000ab11b2ab04210818c1a
Marek Bialoruski <mar.bialoruski@gmail.com>
2022-11-09 04:46:09 -0800

    Further changes to build info

--- app/lib/include/config.hpp
+++ app/lib/include/config.hpp
@@ -12,14 +12,13 @@ struct BuildInformation
   static constexpr std::string_view PROJECT_VERSION{ "0.0.15" };
   static constexpr std::string_view PROJECT_VERSION_ADDENDUM{ "InDev" };
   static constexpr std::string_view GIT_BRANCH{ "dev-windows" };
-  static constexpr std::string_view GIT_BRANCH_STATE{ "DIRTY" };
-  static constexpr std::string_view GIT_SHORT_SHA{ "aa333824" };
+  static constexpr std::string_view GIT_SHORT_SHA{ "b005f238" };
   static constexpr std::string_view BUILD_TYPE{ "Debug" };
   static constexpr std::string_view PLATFORM{ "Windows" };
   static constexpr std::string_view COMPILER{ "GNU" };
   static constexpr std::string_view COMPILER_VERSION{ "12.2.0" };
-  static constexpr std::string_view BUILD_DATE{ "2022-11-09 12:23:44 UTC" };
-  static constexpr std::string_view BUILD_HASH{ "90b58801daf13791d1702f08fb8fe5a14c898840a5c95167a29c245bf2b87bfe" };
+  static constexpr std::string_view BUILD_DATE{ "2022-11-09 12:39:30 UTC" };
+  static constexpr std::string_view BUILD_HASH{ "4c310d35883c445250e09292b9d629ed330b3bf3b909fd20d9547345dc4bfebc" };
 };
 
 static constexpr std::size_t WINDOW_HEIGHT{ 600 };
