4c7b53e55d72653c1706b133dc998d6b5fb2943c
Marek Bialoruski <mar.bialoruski@gmail.com>
2022-11-16 18:25:30 +0100

    WIP draft of SerialReader Possibly will consolidate both platforms into one class

--- app/lib/include/winserialreader.hpp
+++ app/lib/include/winserialreader.hpp
@@ -3,24 +3,50 @@
 
 #pragma once
 
+#include <boost/asio.hpp>
 #include <boost/log/trivial.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/shared_array.hpp>
 #include <windows.h>
 
-#include "serialreader.hpp"
+#include "utilities.hpp"
 
 namespace TimeBox {
 
-class WinSerialReader final : public SerialReader
+class WinSerialReader final : private boost::noncopyable
 {
 public:
   WinSerialReader(const char *, std::size_t, std::function<void(TimeboxReadout)>);
-  ~WinSerialReader() = default;
+  WinSerialReader(std::string, std::size_t, std::function<void(TimeboxReadout)>);
+  virtual ~WinSerialReader() = 0;
+
+  void Open(const std::string &, std::size_t);
+  void Close();
+  bool IsOpen() const;
+  bool ErrorStatus() const;
+
+  void Write(const char *, std::size_t);
+  void Write(const std::vector<char> &);
+  void Write(const std::string &);
+
+  static constexpr std::size_t read_buffer_size{ 512 };
 
 private:
-  HANDLE m_serial_handle;
-  DCB m_serial_parameters{ 0 };
-  void InitializeSerial(const char *, std::size_t) override;
-  void Work() override;
+  void Read();
+
+  boost::asio::io_service m_io_service;
+  std::shared_ptr<boost::asio::serial_port> mp_serial_port;
+  std::thread m_worker_thread;
+  bool m_open;
+  bool m_error_flag;
+  mutable std::mutex m_error_mutex;
+
+  std::vector<char> m_write_queue;
+  boost::shared_array<char> m_write_buffer;
+  std::size_t m_write_buffer_size;
+  std::mutex m_write_queue_mutex;
+
+  std::function<void(TimeboxReadout)> m_callback;
 };
 
 }// namespace TimeBox
--- app/lib/src/winserialreader.cpp
+++ app/lib/src/winserialreader.cpp
@@ -2,21 +2,20 @@
 
 using namespace TimeBox;
 
-WinSerialReader::WinSerialReader(const char *t_com, std::size_t t_baud, std::function<void(TimeboxReadout)> t_callback)
-  : SerialReader(std::move(t_callback))
+WinSerialReader::WinSerialReader(const char *t_device,
+  std::size_t t_baud,
+  std::function<void(TimeboxReadout)> t_callback)
+  : WinSerialReader(std::string(t_device), t_baud, t_callback)
+{}
+
+WinSerialReader::WinSerialReader(std::string t_device,
+  std::size_t t_baud,
+  std::function<void(TimeboxReadout)> t_callback)
 {
-  m_serial_parameters.DCBlength = sizeof(m_serial_parameters);
-};
-
-void WinSerialReader::InitializeSerial(const char *t_com, std::size_t t_baud)
-{
-  size_t size = { strlen(t_com) };
-  wchar_t *warr = new wchar_t[size];
-  for (size_t i = 0; i < size; i++) { warr[i] = t_com[i]; }
-
-  m_serial_handle = CreateFile(warr, GENERIC_READ, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
-  GetCommState(m_serial_handle, &m_serial_parameters);
-
-  m_serial_parameters.BaudRate = ConvertBaudRate(t_baud);
-  m_serial_parameters.ByteSize = 8;
+  using namespace boost::asio;
+  mp_serial_port = std::make_shared<serial_port>(m_io_service);
+  serial_port_base::parity parity{ serial_port_base::parity::none };
+  serial_port_base::character_size character_size{ serial_port_base::character_size(8) };
+  serial_port_base::flow_control flow_control{ serial_port_base::flow_control::none };
+  serial_port_base::stop_bits stop_bits{ serial_port_base::stop_bits(serial_port_base::stop_bits::one) };
 }
