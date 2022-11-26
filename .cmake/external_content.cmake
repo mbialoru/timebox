d58a9b2b8097d40019b8ba52afadb0e60e254f3c
Marek Bialoruski <mar.bialoruski@gmail.com>
2022-11-26 19:35:47 +0100

    WIP CMake Restructure Pre grab

---
+++ .cmake/external_content.cmake
@@ -0,0 +1,65 @@
+FetchContent_Declare(
+  googletest
+  GIT_REPOSITORY https://github.com/google/googletest
+  GIT_BRANCH main
+)
+FetchContent_GetProperties(googletest)
+if(NOT googletest_POPULATED)
+  FetchContent_Populate(googletest)
+  add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
+
+FetchContent_Declare(
+  googlebenchmark
+  GIT_REPOSITORY https://github.com/google/benchmark
+  GIT_BRANCH main
+)
+FetchContent_GetProperties(googlebenchmark)
+if(NOT googlebenchmark_POPULATED)
+  FetchContent_Populate(googlebenchmark)
+  add_subdirectory(${googlebenchmark_SOURCE_DIR} ${googlebenchmark_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
+
+FetchContent_Declare(
+  sdl
+  GIT_REPOSITORY https://github.com/libsdl-org/SDL
+  GIT_BRANCH main
+)
+FetchContent_GetProperties(sdl)
+if(NOT sdl_POPULATED)
+  FetchContent_Populate(sdl)
+  add_subdirectory(${sdl_SOURCE_DIR} ${sdl_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
+
+FetchContent_Declare(
+  imgui
+  GIT_REPOSITORY https://github.com/ocornut/imgui
+  GIT_BRANCH master
+)
+FetchContent_GetProperties(imgui)
+if(NOT imgui_POPULATED)
+  FetchContent_Populate(imgui)
+  add_subdirectory(${imgui_SOURCE_DIR} ${imgui_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
+
+FetchContent_Declare(
+  fmt
+  GIT_REPOSITORY https://github.com/fmtlib/fmt
+  GIT_BRANCH master
+)
+FetchContent_GetProperties(fmt)
+if(NOT fmt_POPULATED)
+  FetchContent_Populate(fmt)
+  add_subdirectory(${fmt_SOURCE_DIR} ${fmt_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
+
+FetchContent_Declare(
+  inja
+  GIT_REPOSITORY https://github.com/pantor/inja
+  GIT_TAG master
+)
+FetchContent_GetProperties(inja)
+if(NOT inja_POPULATED)
+  FetchContent_Populate(inja)
+  add_subdirectory(${inja_SOURCE_DIR} ${inja_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
--- app/lib/include/config.hpp
+++ app/lib/include/config.hpp
@@ -11,14 +11,14 @@ struct BuildInformation
   static constexpr std::string_view PROJECT_NAME{ "timebox" };
   static constexpr std::string_view PROJECT_VERSION{ "0.0.15" };
   static constexpr std::string_view PROJECT_VERSION_ADDENDUM{ "InDev" };
-  static constexpr std::string_view GIT_BRANCH{ "dev" };
-  static constexpr std::string_view GIT_SHORT_SHA{ "891ff650" };
+  static constexpr std::string_view GIT_BRANCH{ "cmake-restructure" };
+  static constexpr std::string_view GIT_SHORT_SHA{ "71caaacc" };
   static constexpr std::string_view BUILD_TYPE{ "Debug" };
   static constexpr std::string_view PLATFORM{ "Windows" };
   static constexpr std::string_view COMPILER{ "GNU" };
   static constexpr std::string_view COMPILER_VERSION{ "12.2.0" };
-  static constexpr std::string_view BUILD_DATE{ "2022-11-26 18:21:57 UTC" };
-  static constexpr std::string_view BUILD_HASH{ "c5a89f1a0d1da64236d42f93732f1fa783f26cbffa3e4f86e9a386e61f3d3369" };
+  static constexpr std::string_view BUILD_DATE{ "2022-11-26 18:35:41 UTC" };
+  static constexpr std::string_view BUILD_HASH{ "5d2f1dfc4a710af6aa06030b1e3ad2e5fa507ee83a3e4f545ad345a4a9928663" };
 };
 
 static constexpr std::size_t WINDOW_HEIGHT{ 600 };
