a99db7e8eaacaca619a416a7b5d422f3b0f82f6b
Marek Bialoruski <mar.bialoruski@gmail.com>
2022-11-26 22:37:52 +0100

    WIP Restructuring CMake adjustment

--- app/lib/include/config.hpp
+++ app/lib/include/config.hpp
@@ -11,14 +11,14 @@ struct BuildInformation
   static constexpr std::string_view PROJECT_NAME{ "timebox" };
   static constexpr std::string_view PROJECT_VERSION{ "0.0.15" };
   static constexpr std::string_view PROJECT_VERSION_ADDENDUM{ "InDev" };
-  static constexpr std::string_view GIT_BRANCH{ "cmake-restructure" };
-  static constexpr std::string_view GIT_SHORT_SHA{ "e06aaaa7" };
+  static constexpr std::string_view GIT_BRANCH{ "dev" };
+  static constexpr std::string_view GIT_SHORT_SHA{ "8f15d261" };
   static constexpr std::string_view BUILD_TYPE{ "Debug" };
   static constexpr std::string_view PLATFORM{ "Windows" };
   static constexpr std::string_view COMPILER{ "GNU" };
   static constexpr std::string_view COMPILER_VERSION{ "12.2.0" };
-  static constexpr std::string_view BUILD_DATE{ "2022-11-26 20:07:37 UTC" };
-  static constexpr std::string_view BUILD_HASH{ "7ca5ee1f6921ac15ac299963526e43a967c62b8a654ca8553737fbb80d33690a" };
+  static constexpr std::string_view BUILD_DATE{ "2022-11-26 21:34:45 UTC" };
+  static constexpr std::string_view BUILD_HASH{ "7303e4c2c8d273dba58a5d6cecc0bbe924ef9df0b330bebdd60aec00959ebb60" };
 };
 
 static constexpr std::size_t WINDOW_HEIGHT{ 600 };
