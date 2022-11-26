8f15d2619d7e613632c515c0b70bf7b0f3777e76
Marek Bialoruski <mar.bialoruski@gmail.com>
2022-11-26 21:11:53 +0100

    Restructured CMake external content acquisition Removed submodules dependency

---
+++ .cmake/ExternalContent.cmake
@@ -0,0 +1,56 @@
+function(DisableAnalyzers target_name)
+  message(STATUS "Disabling static analysis from ProjectOptions for target ${target_name}")
+  target_disable_cpp_check(${target_name})
+  target_disable_vs_analysis(${target_name})
+  target_disable_clang_tidy(${target_name})
+  target_disable_static_analysis(${target_name})
+endfunction(DisableAnalyzers)
+
+function(FetchExternalContent repo_url git_tag content_uid)
+  message("Fetching external repository ${content_uid}")
+  FetchContent_Declare(
+    "${content_uid}"
+    GIT_REPOSITORY "${repo_url}"
+    GIT_TAG "${git_tag}"
+
+    # Commands are left empty so that we only clone source and perform no build
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+  )
+  FetchContent_GetProperties(${content_uid})
+  if(NOT ${content_uid}_POPULATED)
+    FetchContent_Populate(${content_uid})
+    add_subdirectory(${${content_uid}_SOURCE_DIR} ${${content_uid}_BINARY_DIR} EXCLUDE_FROM_ALL)
+  endif()
+  FetchContent_MakeAvailable(${content_uid})
+endfunction(FetchExternalContent)
+
+FetchExternalContent(https://github.com/google/googletest release-1.12.1 googletest)
+DisableAnalyzers(gtest)
+DisableAnalyzers(gtest_main)
+DisableAnalyzers(gmock)
+DisableAnalyzers(gmock_main)
+
+FetchExternalContent(https://github.com/google/benchmark v1.7.1 googlebenchmark)
+DisableAnalyzers(benchmark)
+
+FetchExternalContent(https://github.com/libsdl-org/SDL release-2.26.0 sdl)
+DisableAnalyzers(SDL2)
+
+FetchExternalContent(https://github.com/fmtlib/fmt 9.1.0 fmt)
+DisableAnalyzers(fmt)
+
+FetchExternalContent(https://github.com/pantor/inja v3.3.0 inja)
+DisableAnalyzers(inja)
+
+# FetchContent_Declare(
+#   imgui
+#   GIT_REPOSITORY https://github.com/ocornut/imgui
+#   GIT_TAG v1.89.1
+# )
+# FetchContent_GetProperties(imgui)
+# if(NOT imgui_POPULATED)
+#   FetchContent_Populate(imgui)
+#   add_subdirectory(${imgui_SOURCE_DIR} ${imgui_BINARY_DIR} EXCLUDE_FROM_ALL)
+# endif()
--- app/lib/include/config.hpp
+++ app/lib/include/config.hpp
@@ -12,13 +12,13 @@ struct BuildInformation
   static constexpr std::string_view PROJECT_VERSION{ "0.0.15" };
   static constexpr std::string_view PROJECT_VERSION_ADDENDUM{ "InDev" };
   static constexpr std::string_view GIT_BRANCH{ "cmake-restructure" };
-  static constexpr std::string_view GIT_SHORT_SHA{ "71caaacc" };
+  static constexpr std::string_view GIT_SHORT_SHA{ "e06aaaa7" };
   static constexpr std::string_view BUILD_TYPE{ "Debug" };
   static constexpr std::string_view PLATFORM{ "Windows" };
   static constexpr std::string_view COMPILER{ "GNU" };
   static constexpr std::string_view COMPILER_VERSION{ "12.2.0" };
-  static constexpr std::string_view BUILD_DATE{ "2022-11-26 18:35:41 UTC" };
-  static constexpr std::string_view BUILD_HASH{ "5d2f1dfc4a710af6aa06030b1e3ad2e5fa507ee83a3e4f545ad345a4a9928663" };
+  static constexpr std::string_view BUILD_DATE{ "2022-11-26 20:07:37 UTC" };
+  static constexpr std::string_view BUILD_HASH{ "7ca5ee1f6921ac15ac299963526e43a967c62b8a654ca8553737fbb80d33690a" };
 };
 
 static constexpr std::size_t WINDOW_HEIGHT{ 600 };
