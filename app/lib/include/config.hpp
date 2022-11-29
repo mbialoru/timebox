fcc0101efa3cb7e4b878a467047fd28f30f06d7a
Marek Bialoruski <mar.bialoruski@gmail.com>
2022-11-29 12:12:28 +0100

    CMake restructuring Benchmarks refactor

--- app/lib/include/config.hpp
+++ app/lib/include/config.hpp
@@ -12,13 +12,13 @@ struct BuildInformation
   static constexpr std::string_view PROJECT_VERSION{ "0.0.15" };
   static constexpr std::string_view PROJECT_VERSION_ADDENDUM{ "InDev" };
   static constexpr std::string_view GIT_BRANCH{ "dev" };
-  static constexpr std::string_view GIT_SHORT_SHA{ "cb976080" };
+  static constexpr std::string_view GIT_SHORT_SHA{ "e49be22a" };
   static constexpr std::string_view BUILD_TYPE{ "Debug" };
   static constexpr std::string_view PLATFORM{ "Windows" };
   static constexpr std::string_view COMPILER{ "GNU" };
   static constexpr std::string_view COMPILER_VERSION{ "12.2.0" };
-  static constexpr std::string_view BUILD_DATE{ "2022-11-28 11:35:23 UTC" };
-  static constexpr std::string_view BUILD_HASH{ "ec0bf5ea55e7f7edf779b2ac2df145b11938c48ee1e92f5818e4963205e18014" };
+  static constexpr std::string_view BUILD_DATE{ "2022-11-29 10:58:40 UTC" };
+  static constexpr std::string_view BUILD_HASH{ "2fdf3acb7a1e7a21514be00e4e9fd5541972ec8a7866380baf64abde0aab25d6" };
 };
 
 static constexpr std::size_t WINDOW_HEIGHT{ 600 };
