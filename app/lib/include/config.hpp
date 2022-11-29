5cc79f32687b70453c69525d84d680acbbbf0d64
Marek Bialoruski <mar.bialoruski@gmail.com>
2022-11-29 12:48:28 +0100

    CMake files reformat

--- app/lib/include/config.hpp
+++ app/lib/include/config.hpp
@@ -12,13 +12,13 @@ struct BuildInformation
   static constexpr std::string_view PROJECT_VERSION{ "0.0.15" };
   static constexpr std::string_view PROJECT_VERSION_ADDENDUM{ "InDev" };
   static constexpr std::string_view GIT_BRANCH{ "dev" };
-  static constexpr std::string_view GIT_SHORT_SHA{ "fcc0101e" };
+  static constexpr std::string_view GIT_SHORT_SHA{ "e0d8f430" };
   static constexpr std::string_view BUILD_TYPE{ "Debug" };
   static constexpr std::string_view PLATFORM{ "Windows" };
   static constexpr std::string_view COMPILER{ "GNU" };
   static constexpr std::string_view COMPILER_VERSION{ "12.2.0" };
-  static constexpr std::string_view BUILD_DATE{ "2022-11-29 11:18:26 UTC" };
-  static constexpr std::string_view BUILD_HASH{ "849a7246138ea2af496f503b3717998a9ac61387d5d1e3977c8e71eb1a736007" };
+  static constexpr std::string_view BUILD_DATE{ "2022-11-29 11:44:55 UTC" };
+  static constexpr std::string_view BUILD_HASH{ "328e6a9c0dcfe931194ec18e86ddf8178e5f760b1995ee8a4faa5549ad57ed42" };
 };
 
 static constexpr std::size_t WINDOW_HEIGHT{ 600 };
