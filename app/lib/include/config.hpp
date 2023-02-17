27bfcc0d1b110c35597ca58942e4226e4e41c0ff
Marek Bialoruski <mar.bialoruski@gmail.com>
2023-02-17 14:35:12 +0100

    Migration of arduino code to submodule

--- app/lib/include/config.hpp
+++ app/lib/include/config.hpp
@@ -12,13 +12,13 @@ struct BuildInformation
   static constexpr std::string_view PROJECT_VERSION{ "0.0.15" };
   static constexpr std::string_view PROJECT_VERSION_ADDENDUM{ "InDev" };
   static constexpr std::string_view GIT_BRANCH{ "dev" };
-  static constexpr std::string_view GIT_SHORT_SHA{ "618eb6ea" };
+  static constexpr std::string_view GIT_SHORT_SHA{ "490b6376" };
   static constexpr std::string_view BUILD_TYPE{ "Debug" };
   static constexpr std::string_view PLATFORM{ "Windows" };
   static constexpr std::string_view COMPILER{ "GNU" };
   static constexpr std::string_view COMPILER_VERSION{ "12.2.0" };
-  static constexpr std::string_view BUILD_DATE{ "2023-02-17 09:39:11 UTC" };
-  static constexpr std::string_view BUILD_HASH{ "817dcf4a389536d59c8d3fcd15d1d9e576d1c0c025106193c6eef267aaded324" };
+  static constexpr std::string_view BUILD_DATE{ "2023-02-17 13:28:59 UTC" };
+  static constexpr std::string_view BUILD_HASH{ "93485bdd51e9b6b6bb03ec3b8d7d8d76fd100c4e51d5718a8766e006c2ef992b" };
 };
 
 static constexpr std::size_t WINDOW_HEIGHT{ 600 };
