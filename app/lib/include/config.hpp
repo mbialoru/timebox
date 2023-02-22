f0d81a79e4917bd7f86808f3e9686667e13885f4
Marek Bialoruski <mar.bialoruski@gmail.com>
2023-02-22 13:36:58 +0100

    Prototypes restructuring

--- app/lib/include/config.hpp
+++ app/lib/include/config.hpp
@@ -12,13 +12,13 @@ struct BuildInformation
   static constexpr std::string_view PROJECT_VERSION{ "0.0.15" };
   static constexpr std::string_view PROJECT_VERSION_ADDENDUM{ "InDev" };
   static constexpr std::string_view GIT_BRANCH{ "dev" };
-  static constexpr std::string_view GIT_SHORT_SHA{ "4d95690f" };
+  static constexpr std::string_view GIT_SHORT_SHA{ "3a46ea32" };
   static constexpr std::string_view BUILD_TYPE{ "Debug" };
   static constexpr std::string_view PLATFORM{ "Windows" };
   static constexpr std::string_view COMPILER{ "GNU" };
   static constexpr std::string_view COMPILER_VERSION{ "12.2.0" };
-  static constexpr std::string_view BUILD_DATE{ "2023-02-22 12:10:09 UTC" };
-  static constexpr std::string_view BUILD_HASH{ "947f62d13e579dfadc202bd29e460001c4523fd538f2a6cac9617c7548e5a75c" };
+  static constexpr std::string_view BUILD_DATE{ "2023-02-22 12:29:07 UTC" };
+  static constexpr std::string_view BUILD_HASH{ "7896b8d64ad0f77c852a4a7798e3296e3d160f6f6ad9554f5557dbfa7191bfea" };
 };
 
 static constexpr std::size_t WINDOW_HEIGHT{ 600 };
